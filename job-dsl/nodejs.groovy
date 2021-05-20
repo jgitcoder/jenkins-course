05ab67f185482a549c1622488bee36a9a4a1d86c
jgitcoder <36025628+jgitcoder@users.noreply.github.com>
2021-05-20 19:00:08 +0900

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
-job('NodeJS Docker example') {
+job('NodeJS example') {
     scm {
-        git('https://github.com/jgitcoder/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/arjunachari12/docker-demo.git') {  node -> 
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
@@ -9,18 +9,10 @@ job('NodeJS Docker example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+        nodejs('nodejs') 
+                         
     }
     steps {
-        dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
-            forcePull(false)
-            forceTag(false)
-            createFingerprints(false)
-            skipDecorate()
-        }
+        shell("npm install")
     }
 }
